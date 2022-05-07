e7ac32562617efbc3fe3f73aadd1736416a6aa92
psanjayvarma <sanjaypenmetsa@yahoo.com>
2022-05-06 17:38:27 -0700

    add view board functionality and User Login Session

--- src/main/java/edu/uwb/project/encryptedbulletinboard/controller/MainController.java
+++ src/main/java/edu/uwb/project/encryptedbulletinboard/controller/MainController.java
@@ -4,14 +4,15 @@ import edu.uwb.project.encryptedbulletinboard.model.BoardModel;
 import edu.uwb.project.encryptedbulletinboard.model.UserModel;
 import edu.uwb.project.encryptedbulletinboard.service.BoardService;
 import edu.uwb.project.encryptedbulletinboard.service.UserService;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class MainController {
-
-    UserModel authenticated = new UserModel();
 
     private final UserService userService;
     private final BoardService boardService;
@@ -21,69 +22,24 @@ public class MainController {
         this.boardService = boardService;
     }
 
+    // GET and POST for login/index page
     @GetMapping("/")
-    public String getIndexPage(Model model){
-        if(authenticated.getLogin() == null){
-            return getLoginPage(model);
+    public String getIndexPage(HttpSession session, Model model){
+        if(session.getAttribute("user") == null || session.getAttribute("user").equals("")){
+            session.setAttribute("user", new UserModel());
+            return "login_page";
         }
-        model.addAttribute("user", authenticated);
+        System.out.println("Session found ...." + session.getAttribute("user"));
         return "welcome_page";
+
     }
 
-    @GetMapping("/welcome")
-    public String getHomePage(Model model){
-        model.addAttribute("user", authenticated);
-        return "welcome_page";
-    }
-
-    @GetMapping("/register")
-    public String getRegisterPage(Model model){
-        model.addAttribute("registerRequest", new UserModel());
-        return "register_page";
-    }
-
-    @GetMapping("/login")
-    public String getLoginPage(Model model){
-        model.addAttribute("loginRequest", new UserModel());
-        return "login_page";
-    }
-
-    @GetMapping("/createboard")
-    public String getCreateBoardPage(Model model){
-        model.addAttribute("board", new BoardModel());
-        return "create_board";
-    }
-
-    @GetMapping("/joinboard")
-    public String getJoinBoardPage(Model model){
-        model.addAttribute("Id", 0);
-        return "join_board";
-    }
-
-    @GetMapping("/signout")
-    public String getSingOutPage(Model model){
-        authenticated = new UserModel();
-        return "redirect:/";
-    }
-
-    @PostMapping("/register")
-    public String register(@ModelAttribute UserModel userModel, Model model){
-        System.out.println("Register request for ...." + userModel);
-        UserModel registeredUser = userService.registerUser(userModel.getLogin(), userModel.getPassword(), userModel.getEmail());
-        if(registeredUser == null){
-            model.addAttribute("registrationError", "User Already Exists");
-            return "register_page";
-        }
-        model.addAttribute("userRegistered","New User Registered Successfully. Login with using Credentials");
-        return "login_page";
-    }
-
-    @PostMapping("/login")
-    public String login(@ModelAttribute UserModel userModel, Model model){
-        System.out.println("Register request for ...." + userModel);
-        authenticated = userService.authenticate(userModel.getLogin(), userModel.getPassword());
-        if(authenticated != null){
-            model.addAttribute("user", authenticated);
+    @PostMapping("/")
+    public String login(@ModelAttribute UserModel userModel, HttpSession session, Model model){
+        session.setAttribute("user", userModel);
+        UserModel user = userService.authenticate(userModel.getLogin(), userModel.getPassword());
+        if(user != null){
+            session.setAttribute("user", user);
             return "welcome_page";
         }
         else{
@@ -92,40 +48,82 @@ public class MainController {
         }
     }
 
+    // GET and POST for register page
+    @GetMapping("/register")
+    public String getRegisterPage(Model model){
+        model.addAttribute("registerRequest", new UserModel());
+        return "register_page";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute UserModel userModel, Model model){
+        UserModel registeredUser = userService.registerUser(userModel.getLogin(), userModel.getPassword(), userModel.getEmail());
+        System.out.println("Register request for ...." + registeredUser);
+        if(registeredUser == null){
+            model.addAttribute("registrationError", "User Already Exists");
+            return "register_page";
+        }
+        model.addAttribute("userRegistered","New User Registered Successfully. Login with using Credentials");
+        return "login_page";
+    }
+
+    // Function to sign out from the session
+    @GetMapping("/signout")
+    public String getSingOutPage(HttpSession session, Model model){
+        session.setAttribute("user", null);
+        return "redirect:/";
+    }
+
+    // GET and POST for creating a new board
+    @GetMapping("/createboard")
+    public String getCreateBoardPage(Model model){
+        model.addAttribute("board", new BoardModel());
+        return "create_board";
+    }
+
     @PostMapping("/createboard")
-    public String createNewBoard(@ModelAttribute UserModel userModel, BoardModel boardModel, Model model){
+    public String createNewBoard(@ModelAttribute UserModel userModel, BoardModel boardModel, HttpSession session, Model model){
+        UserModel user = (UserModel) session.getAttribute("user");
         BoardModel newBoard = boardService.createNewBoard(boardModel.getName());
-        System.out.println("New Board Creation request for ...." + newBoard +"by user "+ authenticated);
-        authenticated = userService.addNewBoard(authenticated, newBoard);
-        model.addAttribute("user", authenticated);
+        System.out.println("New Board Creation request for ...." + newBoard +"by user "+ user);
+        user = userService.addNewBoard(user, newBoard);
+        model.addAttribute("user", user);
         return "welcome_page";
     }
 
+    // GET and POST for joining a board
+    @GetMapping("/joinboard")
+    public String getJoinBoardPage(Model model){
+        model.addAttribute("Id", 0);
+        return "join_board";
+    }
 
     @PostMapping("/joinboard")
-    public String joinBoard(@ModelAttribute UserModel userModel, Integer Id, Model model){
+    public String joinBoard(@ModelAttribute UserModel userModel, Integer Id,HttpSession session, Model model){
+        UserModel user = (UserModel) session.getAttribute("user");
         BoardModel board = boardService.getBoard(Id);
-        System.out.println("Join Request for ......" + board + " by " + authenticated);
+        System.out.println("Join Request for ......" + board + " by " + user);
         if(board == null){
             System.out.println("Board ID is not valid");
             model.addAttribute("JoinError", "Board Doesn't Exist");
             return "join_board";
         }
-        if(userService.hasTheBoard(authenticated,Id)){
+        if(userService.hasTheBoard(user,Id)){
             System.out.println("User is already in the board");
             model.addAttribute("JoinError", "You already joined the Board");
             return "join_board";
         }
-        authenticated = userService.addNewBoard(authenticated, board);
-        model.addAttribute("user", authenticated);
+        user = userService.addNewBoard(user, board);
+        model.addAttribute("user", user);
         return "welcome_page";
     }
 
+
+    //Load a board
     @GetMapping("/board/{board_id}")
     public String viewBoardPage(@PathVariable("board_id") Integer Id, Model model){
         BoardModel boardModel = boardService.getBoard(Id);
         model.addAttribute("board", boardModel);
         return "view_board";
     }
-
 }
