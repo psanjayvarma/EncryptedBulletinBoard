e54da4b33e4598b546040aa378cb20a765c1a130
Anirudh Potturi <anirudhp1099@gmail.com>
2022-05-12 18:26:12 -0700

    Added required constraint for fields like Enter BoardID , Username, Password etc.

--- src/main/java/edu/uwb/project/encryptedbulletinboard/repository/UserRepository.java
+++ src/main/java/edu/uwb/project/encryptedbulletinboard/repository/UserRepository.java
@@ -2,9 +2,13 @@ package edu.uwb.project.encryptedbulletinboard.repository;
 
 import edu.uwb.project.encryptedbulletinboard.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
+@EnableJpaRepositories
 public interface UserRepository extends JpaRepository<UserModel, Integer> {
 
     Optional<UserModel> findByLoginAndPassword(String login, String password);
